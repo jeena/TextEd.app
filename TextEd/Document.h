ba9027c7ffe860034b6db1df043d573296e1a1f2
Jeena <spam@jeenaparadies.net>
2014-01-21 21:37:18 +0100

    Initial Commit

---
+++ TextEd/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  TextEd
+//
+//  Created by Jeena on 2014-01-21.
+//  Copyright (c) 2014 Jeena. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ TextEd/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TextEd
+//
+//  Created by Jeena on 2014-01-21.
+//  Copyright (c) 2014 Jeena. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
